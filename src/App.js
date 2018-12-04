ba75b5308345100b1ef42eabd24ea25ac7665a74
Carlos Rodriguez <carlos.a.rodriguez100@gmail.com>
2018-12-03 20:17:18 -0500

    inital practice

--- src/App.js
+++ src/App.js
@@ -3,26 +3,59 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+  this.state = {
+    scores: [
+      {
+        id:1,
+        name: "carlos",
+        score: 1001
+      },
+      {
+        id:2,
+        name: "carlos2",
+        score: 100
+      },
+      {
+        id:3,
+        name: "carlos3",
+        score: 1
+      },
+    ]
+  }
+
+  }
+  
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Scoreboard App</h1>
+        <div className="score-container">
+        {this._scoresAsCards()}
+        
+        </div>
       </div>
     );
   }
+
+  _scoresAsCards(){
+    const cards = this.state.scores.map(score => {
+      return (<div key={score.id}> 
+        <h2>Name: {score.name} // Score: {score.score}</h2>
+        <button onClick={() => this._incrementScoreById(score.id)}> + </button>
+      </div>)
+    });
+    return cards
+  };
+
+_incrementScoreById(id){
+  
+
+}
+
 }
 
 export default App;
